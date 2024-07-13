4302c1985a79f3e70c7711f84a11503305cbda04
victory balogun <victorybalogun@gmail.com>
2024-07-13 16:30:06 +0100

    New Commit

---
+++ src/pages/assessments/Lay.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Lay = () => {
+    const [lay, setLay] = React.useState('Lay')
+
+  return (
+    <div className='h-0 w-2'>Lay</div>
+  )
+}
+
+export default Lay
